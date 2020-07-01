ea3e2624269710baa3962c448143965f048f813d
Rajesh Sharma <apka.rajesh@hotmail.com>
2020-07-01 15:15:45 +0530

    checking in

---
+++ CRUDMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,129 @@
+﻿using System.Web.Mvc;
+using CRUDMVC.Models;
+namespace CRUDMVC.Controllers
+{
+
+    public class EmployeeController : Controller
+    {
+
+        /// <summary>
+        // GET: Employee/GetAllEmpDetails
+        /// Get all the employee details
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetAllEmpDetails()
+        {
+            ViewBag.Title = "Get All Employee";
+            DBOps.DBOps EmpRepo = new DBOps.DBOps();
+            ModelState.Clear();
+            return View(EmpRepo.GetAllEmployees());
+        }
+
+        /// <summary>
+        /// GET: Employee/AddEmployee
+        /// add employee
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddEmployee()
+        {
+            ViewBag.Title = "Add Employee";
+            return View();
+        }
+
+
+        /// <summary>
+        ///  POST: Employee/AddEmployee
+        /// </summary>
+        /// <param name="Emp">object of employee</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult AddEmployee(EmpModel Emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    DBOps.DBOps EmpRepo = new DBOps.DBOps();
+
+                    if (EmpRepo.AddEmployee(Emp))
+                    {
+                        ViewBag.Message = "Employee details added successfully";
+                    }
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        /// <summary>
+        /// edit the selected employee
+        /// GET: Employee/EditEmpDetails/1
+        /// </summary>
+        /// <param name="id">id of employee</param>
+        /// <returns></returns>
+        public ActionResult EditEmpDetails(int id)
+        {
+            ViewBag.Title = "Edit Employee";
+            DBOps.DBOps EmpRepo = new DBOps.DBOps();
+            return View(EmpRepo.GetAllEmployees().Find(Emp => Emp.Empid == id));
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// POST: Employee/EditEmpDetails/5
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [HttpPost]
+
+        public ActionResult EditEmpDetails(int id, EmpModel obj)
+        {
+            try
+            {
+                DBOps.DBOps EmpRepo = new DBOps.DBOps();
+
+                EmpRepo.UpdateEmployee(obj);
+                return RedirectToAction("GetAllEmpDetails");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        /// <summary>
+        ///  GET: Employee/DeleteEmp/5
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns></returns>
+        public ActionResult DeleteEmp(int id)
+        {
+            try
+            {
+                DBOps.DBOps EmpRepo = new DBOps.DBOps();
+                if (EmpRepo.DeleteEmployee(id))
+                {
+                    ViewBag.AlertMsg = "Employee details deleted successfully";
+
+                }
+                return RedirectToAction("GetAllEmpDetails");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ CRUDMVC/DBOps/DBOps.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using CRUDMVC.Models;
+using System.Linq;
+
+namespace CRUDMVC.DBOps
+{
+    public class DBOps
+    {
+
+        private SqlConnection con;
+       /// <summary>
+       /// Get the connetion string from the Database
+       /// </summary>
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["dbconnect"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+
+        /// <summary>
+        /// To Add Employee details
+        /// </summary>
+        /// <param name="obj">Object of Employee</param>
+        /// <returns>boolean value</returns>
+        public bool AddEmployee(EmpModel obj)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("AddNewEmpDetails", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@Name", obj.Name);
+            com.Parameters.AddWithValue("@City", obj.City);
+            com.Parameters.AddWithValue("@Address", obj.Address);
+          
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+            }
+
+
+        }
+     
+        /// <summary>
+        /// To view employee details with generic list 
+        /// </summary>
+        /// <returns>List of All Employee</returns>
+      
+        public List<EmpModel> GetAllEmployees()
+        {
+            // uncommented the portion if we have database connected.
+
+            connection();
+            List<EmpModel> EmpList =new List<EmpModel>();
+            //SqlCommand com = new SqlCommand("GetEmployees", con);
+            //com.CommandType = CommandType.StoredProcedure;
+            //SqlDataAdapter da = new SqlDataAdapter(com);
+            //DataTable dt = new DataTable();
+            //con.Open();
+            DataTable dt = DummyData();
+            //da.Fill(dt);
+            //con.Close();
+           
+            //  Bind EmpModel generic list using dataRow
+            foreach (DataRow dr in dt.Rows)
+                {
+
+                    EmpList.Add(
+
+                        new EmpModel
+                        {
+
+                            Empid = Convert.ToInt32(dr["Id"]),
+                            Name = Convert.ToString(dr["Name"]),
+                            City = Convert.ToString(dr["City"]),
+                            Address = Convert.ToString(dr["Address"])
+
+                        }
+
+
+                        );
+
+
+                }
+
+            return EmpList;
+
+
+        }
+
+        /// <summary>
+        /// To Update Employee details
+        /// </summary>
+        /// <param name="obj">Employee object</param>
+        /// <returns>Return true/false based on update </returns>
+        public bool UpdateEmployee(EmpModel obj)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("UpdateEmpDetails", con);
+           
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@EmpId", obj.Empid);
+            com.Parameters.AddWithValue("@Name", obj.Name);
+            com.Parameters.AddWithValue("@City", obj.City);
+            com.Parameters.AddWithValue("@Address", obj.Address);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+            }
+
+
+        }
+
+        /// <summary>
+        /// To delete Employee details
+        /// </summary>
+        /// <param name="Id">employee id</param>
+        /// <returns></returns>
+        public bool DeleteEmployee(int Id)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("DeleteEmpById", con);
+
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@EmpId", Id);
+           
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+               
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+            }
+
+
+        }
+
+        /// <summary>
+        /// This is dummy datatable to show the list of employee
+        /// </summary>
+        /// <returns></returns>
+        private DataTable DummyData()
+        {
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("City");
+            dt.Columns.Add("Address");
+            dt.Rows.Add("1", "emp1", "City1", "address1");
+            dt.Rows.Add("2", "emp1", "City1", "address1");
+            dt.Rows.Add("3", "emp1", "City1", "address1");
+            dt.Rows.Add("4", "emp1", "City1", "address1");
+            dt.Rows.Add("5", "emp1", "City1", "address1");
+            dt.Rows.Add("6", "emp1", "City1", "address1");
+            return dt;
+
+        }
+    }
+}
